413be1553e5460859de5cbd224bc014e24b4cff8
Marius Sarca <marius.sarca@gmail.com>
2016-12-31 00:17:39 +0200

    Added autoload file

---
+++ autoload.php
@@ -0,0 +1,43 @@
+<?php
+/* ===========================================================================
+ * Copyright 2013-2016 The Opis Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============================================================================ */
+
+spl_autoload_register(function ($class) {
+
+    $class = ltrim($class, '\\');
+    $dir = __DIR__ . '/src';
+    $namespace = 'Opis\ORM';
+
+    if (strpos($class, $namespace) === 0) {
+        $class = substr($class, strlen($namespace));
+        $path = '';
+        if (($pos = strripos($class, '\\')) !== false) {
+            $path = str_replace('\\', '/', substr($class, 0, $pos)) . '/';
+            $class = substr($class, $pos + 1);
+        }
+        $path .= str_replace('_', '/', $class) . '.php';
+        $dir .= '/' . $path;
+
+        if (file_exists($dir)) {
+            include $dir;
+            return true;
+        }
+
+        return false;
+    }
+
+    return false;
+});
