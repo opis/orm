79ffd44c381060f233b762314ac1ed75aabba6e3
Marius Sarca <marius.sarca@gmail.com>
2018-05-18 21:49:44 +0300

    Added methods

--- src/IEntityMapper.php
+++ src/IEntityMapper.php
@@ -17,8 +17,94 @@
 
 namespace Opis\ORM;
 
+use Opis\ORM\Core\RelationFactory;
 
 interface IEntityMapper
 {
+    /**
+     * @param string $class
+     * @return IEntityMapper
+     */
+    public function entityClass(string $class): self;
 
+    /**
+     * @param string $table
+     * @return IEntityMapper
+     */
+    public function table(string $table): self;
+
+    /**
+     * @param string ...$primaryKey
+     * @return IEntityMapper
+     */
+    public function primaryKey(string ...$primaryKey): self;
+
+    /**
+     * @param callable $callback
+     * @return IEntityMapper
+     */
+    public function primaryKeyGenerator(callable $callback): self;
+
+    /**
+     * @param string $sequence
+     * @return IEntityMapper
+     */
+    public function sequence(string $sequence): self;
+
+    /**
+     * @param string $column
+     * @param callable $callback
+     * @return IEntityMapper
+     */
+    public function getter(string $column, callable $callback): self;
+
+    /**
+     * @param string $column
+     * @param callable $callback
+     * @return IEntityMapper
+     */
+    public function setter(string $column, callable $callback): self;
+
+    /**
+     * @param string $name
+     * @return RelationFactory
+     */
+    public function relation(string $name): RelationFactory;
+
+    /**
+     * @param array $casts
+     * @return IEntityMapper
+     */
+    public function cast(array $casts): self;
+
+    /**
+     * @param bool $value
+     * @return IEntityMapper
+     */
+    public function useSoftDelete(bool $value = true): self;
+
+    /**
+     * @param bool $value
+     * @return IEntityMapper
+     */
+    public function useTimestamp(bool $value = true): self;
+
+    /**
+     * @param string[] $columns
+     * @return IEntityMapper
+     */
+    public function fillable(array $columns): self;
+
+    /**
+     * @param string[] $columns
+     * @return IEntityMapper
+     */
+    public function guarded(array $columns): self;
+
+    /**
+     * @param string $name
+     * @param callable $callback
+     * @return IEntityMapper
+     */
+    public function filter(string $name, callable $callback): self;
 }
